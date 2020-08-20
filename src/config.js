2afd31da617943f22cce9eb56050b55c71f51f00
unknown <justin.yeh@aya.yale.edu>
2020-08-20 10:56:09 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "coffee-chat-app-api-dev-serverlessdeploymentbucke-4tcviv0itrnz"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://jrew0lokb9.execute-api.us-west-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_RzXd2yn2s",
+    APP_CLIENT_ID: "1h964kvm8pq097fnajuj3nj8pi",
+    IDENTITY_POOL_ID: "us-west-2:01cfb061-37cf-4e75-b63b-5cfe063e0db3"
+  }
+};
