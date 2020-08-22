20b23c5bdd35dc604a9118f861d169ead1f143a1
unknown <justin.yeh@aya.yale.edu>
2020-08-22 13:51:44 -0400

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import ResetPassword from "./containers/ResetPassword";
 import NotFound from "./containers/NotFound";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
@@ -9,7 +14,15 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
-
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/login/reset">
+        <ResetPassword />
+      </UnauthenticatedRoute>
       <Route>
         <NotFound />
       </Route>
